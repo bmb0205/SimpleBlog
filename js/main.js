fcaa8b27e8d23c3f56438b91ff2f6951574d930f
bmb0205 <bburciag@uncc.edu>
2016-06-22 08:54:26 -0400

    feat: add as many features as possible in time allowed, forgive the lack of commit detail here

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@
  * the user password is simply the user name + "123".
  */
 var app = angular.module('simpleblog', [
-    'ngRoute'
+    'ngRoute',
+    'bootstrapLightbox'
 ]);
 
 /**
@@ -15,10 +16,10 @@ var app = angular.module('simpleblog', [
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     // Home
-        .when("/", { templateUrl: "views/welcome.html", controller: "PageCtrl" })
+        .when("/", { templateUrl: "views/login.html", controller: "FormCtrl" })
 
         // Blog
-        .when("/blog", { templateUrl: "views/blog.html", controller: "PageCtrl" })
+        .when("/blog", { templateUrl: "views/blog.html", controller: "BlogCtrl" })
 
         // Photos
         .when("/photos", { templateUrl: "views/photos.html", controller: "PhotoCtrl" })
@@ -26,12 +27,32 @@ app.config(['$routeProvider', function($routeProvider) {
         // Comments
         // TODO: comments page. decide best design for best user experience
         .when("/comments", { templateUrl: "views/comments.html", controller: "PageCtrl" })
-
-
+          
     // else 404
         // TODO: 404 page
     .otherwise("/404", { templateUrl: "views/404.html", controller: "PageCtrl" });
+
 }]);
+
+/**
+ * Controls log-in welcome screen
+ * TODO: make this a modal, hide password characters, send user name to header template for welcome display in top corner
+ */
+app.controller('FormCtrl', function ($scope, $location) {
+  console.log("Form controller hit");
+  this.username = "Guest";
+
+  $scope.login = function() {
+    if($scope.password === $scope.username + "123") {
+      alert("Thanks for logging in!");
+      $location.path('/blog');
+    } else {
+      alert("Sorry, your password is incorrect!")
+    }
+    console.log($scope.username);
+    console.log($scope.password);
+  };
+});
 
 /**
  * Controls blog, comments supplied by JSONPlaceHolder API
@@ -40,30 +61,56 @@ app.controller('BlogCtrl', function($scope, $http) {
     console.log("Blog Controller hit.");
     $http.get('http://jsonplaceholder.typicode.com/posts')
         .then(function(response) {
-            $scope.blog = response.data.slice(0, 15);
+
+          // Placeholder number of blog posts
+          // TODO: add pagination
+          $scope.blog = response.data.slice(0, 50);
+
+          // TODO: put in method, get all comments for specific blog posts on "view comments" click
+          // $http.get('http://jsonplaceholder.typicode.com/comments')
+          //   .then(function(response) {
+          //     $scope.comments = response.data;
+          //   });
+
         }, function errorCallBack(response) {
             console.log(response);
         });
+
+    // TODO: fix to create fake POST request to retrieve specific blog post and comments
+    function viewComments($scope, id) {
+      alert(id);
+      $http.get('http://jsonplaceholder.typicode.com/posts?userId=' + id)
+        .then(function(response) {
+          alert(response.data);
+        });
+    }
+});
+
+/**
+ * Controls photos supplied by JSONPlaceHolder API.
+ * User can select number of images to display on the page.
+ * CLicking on an image displays a responsive modal you can click or keypress through
+ * TODO: add image specific details to modal content, add pagination to page
+ */
+app.controller('PhotoCtrl', function($scope, $http, Lightbox) {
+    console.log("hit photocontroller");
+
+    // TODO: POST image display count and set it in slice below
+    $http.get('http://jsonplaceholder.typicode.com/photos')
+        .then(function(response) {
+            $scope.images = response.data.slice(0, 50);
+        }, function errorCallBack(response) {
+            console.log(response);
+        });
+
+    $scope.openLightboxModal = function (index, title) {
+        Lightbox.openModal($scope.images, index);
+    };
 });
 
 /**
  * Controls welcome, 404
  */
 app.controller('PageCtrl', function() {
-    console.log("Page Controller hit.");
-});
-
-/**
- * Controls photos supplied by JSONPlaceHolder API.
- * User can select number of images to display on the page
- */
-app.controller('PhotoCtrl', function($scope, $http) {
-    console.log("hit photocontroller");
-    // TODO: POST image display count and set it in slice below
-    $http.get('http://jsonplaceholder.typicode.com/photos')
-        .then(function(response) {
-            $scope.images = response.data.slice(0, 15);
-        }, function errorCallBack(response) {
-            console.log(response);
-        });
+  console.log("Page Controller hit.");
 });
